f59a4dc4c4b4652a5fb6b0141dad25e26816af32
Juan Pablo <gonzalez0421200@gmail.com>
2025-02-01 19:31:04 -0600

    Primer commit - Subiendo mi App de instagram

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  heartOutline
+} from 'ionicons/icons';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonButton,
+    IonIcon,
+    ExploreContainerComponent,
+  ],
+})
+export class Tab1Page {
+  constructor() {
+    addIcons({
+      'heart-outline': heartOutline,
+    });
+  }
+}
